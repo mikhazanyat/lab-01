44f781e21d191bbeb4a9bcef5838901f67f377d6
mikhazanyat <125156037+mikhazanyat@users.noreply.github.com>
2023-09-22 19:39:14 +0300

    first commit

---
+++ Hello world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+<<<<<<< HEAD
+int
+main(int argc, char** argv)
+{
+  string name;                                      // Name of @user
+  std::cin >> name;                                 // Input name of @user
+  std::cout << "Hello world from " << name << endl; // Output name of @user
+=======
+int main (int argc, char** argv) {
+    string name;                                      // Имя пользователя
+    std::cin >> name;                                 // Ввод имени пользователя
+    std::cout << "Hello world from " << name << endl; // Вывод данных
+>>>>>>> master
+}
